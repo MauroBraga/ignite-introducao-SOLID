8a923cf028161594b2bd22fb9763032ec3406f39
MauroBraga <M@r202150016774>
2021-06-14 09:55:47 -0300

    fim teste

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  handle(request: Request, response: Response): Response<User[]> {
+    const { user_id } = request.headers;
+
+    let users;
+
+    try {
+      users = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
+      return response.status(202).json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  handle(request: Request, response: Response): Response<User> {
+    const { user_id } = request.params;
+    try {
+      const user = this.showUserProfileUseCase.execute({ user_id });
+      return response.json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
